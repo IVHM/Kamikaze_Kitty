bdb8b4ad4b7843c5b0ec373893b8b3900d45e1b0
hmatthyseniv <hmatthyseniv@penguin>
2020-03-13 13:23:03 -0500

    Add: collision methods

--- collision.lua
+++ collision.lua
@@ -2,43 +2,55 @@ HC = require "HC"
 
 --world = HC.new(64)
 
-objects = {}
-crnt_id = 0
+col_objects = {}
+crnt_ID = 0
 
-function objects:add_object(shape,info)
-	crnt_id = crnt_id + 1
+function col_objects:add_object(shape,info)
+	crnt_ID = crnt_ID + 1
 	
 	--If new shape is rect info is x,y,w,h values
 	if shape == "rect" then
 		if #info == 4 then
-			objects[crnt_id] = HC.rectangle(info[1], info[2],
+			col_objects[crnt_ID] = HC.rectangle(info[1], info[2],
 											info[3], info[4])
 		else
 			add_object_ERROR(info)
-			crnt_id = crnt_id -1 
+			crnt_ID = crnt_ID -1 
 		end
 	--If new shape is a point info is x,y values
 	else if shape == "point" then
 		if #info == 2 then
-			objects[crnt_id] = HC.point(info[1],info[2])
+			col_objects[crnt_ID] = HC.point(info[1],info[2])
 		else
-			add_object_ERROR(shape, info, crnt_id)
-			crnt_id = crnt_id -1
+			add_object_ERROR(shape, info, crnt_ID)
+			crnt_ID = crnt_ID -1
 		end
 	else 
 		print("unrecognized shape of :"..shape)
 	end
 
 	
-	return crnt_id
+	return crnt_ID
 end
 
 -- Used to output debug error messages
-function add_object_ERROR(shape_in,info_in, crnt_id_in)
-	print("Wrong number of values in info for new shape: "..shape_in..crnt_id_in)
+function add_object_ERROR(shape_in,info_in, crnt_ID_in)
+	print("Wrong number of values in info for new shape: "..shape_in..crnt_ID_in)
 	print("Info contained "..#info_in.." elements.")
 	for i=1,#info_in do
 		print(i..") info : "..info_in[i])
 	end
 
 end
+
+function col_objects:get_collisions(object_ID)
+	local colisions_detected = HC.collisions(col_objects[object_ID])
+	if #colisions_detected == 0 then
+		return false 
+	else
+		return colisions_detected
+	end
+end
+
+
+
